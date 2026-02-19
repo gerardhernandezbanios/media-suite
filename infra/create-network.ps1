4b03881bd9d98e0a0865cd47e91f3cb1f9e78ff7
Gerard <gerard.hernandez@gmail.com>
2026-02-19 19:34:58 +0100

    Added bypass to execute ps1 on windows11. Changed the postgres port expoed to the host.

--- infra/create-network.ps1
+++ infra/create-network.ps1
@@ -1,3 +1,8 @@
+# This script creates a Docker network for the media suite containers.
+
+# Note: You may need to run this script with elevated privileges (Run as Administrator) to set the execution policy.
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
 $networkName = "media-suite-net"
 
 if (!(docker network ls --format "{{.Name}}" | Select-String -Pattern $networkName)) {
